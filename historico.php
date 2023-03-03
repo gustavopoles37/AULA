583947de790ea6e6c7e1de67975bc162b62eb0c4
gustavopoles37 <101608591+gustavopoles37@users.noreply.github.com>
2023-03-02 22:33:57 -0300

    Add files via upload

---
+++ historico.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/historico/historico.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <h1>Histórico de veiculos</h1>
+        <div class="div-linha">
+            <fieldset class="first-fiel">
+                <legend class="legenda-1">Hoje </legend>
+                <div class="div-1"></div>
+                    <p class="p-placa">Placa: ZYK4R53 - (carro) - e: 09:40 | s:10:32</p>
+                <div class="div-2">
+                    <p class="p-placa">Placa: ZYK4R53 - (carro) - e: 09:40 | s:10:32</p>
+                </div>
+
+            </fieldset>
+            <fieldset class="first-fiel">
+                <legend class="legenda-1">Hoje </legend>
+                <div class="div-1"></div>
+                    <p class="p-placa">Placa: ZYK4R53 - (carro) - e: 09:40 | s:10:32</p>
+                <div class="div-2">
+                    <p class="p-placa">Placa: ZYK4R53 - (carro) - e: 09:40 | s:10:32</p>
+                </div>
+            </fieldset>
+        </div>
+    </main>
+</body>
+</html>
